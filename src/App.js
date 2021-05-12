1397ce91d91e8594c65460bd1c707f800af0b200
Baptiste Dauphouy <bdauphouy@gmail.com>
2021-05-12 18:05:50 +0200

    typo corrected

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ const App = () => {
     <>
       <h1>Welcome !</h1>
       <p>I'm Baptiste Dauphouy, a french frontend developer</p>
+      <p>Work in progress</p>
       <ul>
         <li>
           <a href="/cv">My CV</a>
         </li>
         <li>
-          <a href="/projet-etudes">Studied project</a>
+          <a href="/projet-etudes">Studies project</a>
         </li>
         <li>
           <a href="/benjamin-code">One of my project</a>
