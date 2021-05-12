6b74f56a728c6aaced4456293274b5e1a40f13e5
Baptiste Dauphouy <bdauphouy@gmail.com>
2021-05-12 20:09:38 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,110 @@
+import styled from 'styled-components'
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome !</h1>
-      <p>I'm Baptiste Dauphouy, a french frontend developer</p>
-      <p>Work in progress</p>
-      <ul>
-        <li>
-          <a href="/cv">My CV</a>
-        </li>
-        <li>
-          <a href="/projet-etudes">Studies project</a>
-        </li>
-        <li>
-          <a href="/benjamin-code">One of my project</a>
-        </li>
-      </ul>
-    </>
+    <Container>
+      <Title>Baptiste Dauphouy</Title>
+      <Subtitle>
+        Bonjour ! Je m'appelle Baptiste, j'ai 17 ans et je suis actuellement en
+        classe de terminale.
+        <br />
+        Je me passionne pour le développement web depuis maintenant plus de 2
+        ans. C'est pourquoi je rejoindrai dès l'année prochaine l'école HETIC,
+        qui forme sur les métiers et technologies du web.
+      </Subtitle>
+      <List>
+        <Link>
+          <a href="/cv" data-text="CV">
+            CV
+          </a>
+        </Link>
+        <Link>
+          <a href="/projet-etudes" data-text="Projet d'études">
+            Projet d'études
+          </a>
+        </Link>
+        <Link>
+          <a href="/cours" data-text="Cours">
+            Cours
+          </a>
+        </Link>
+        <Link>
+          <a href="https://twitter.com/baptistedph" data-text="Twitter">
+            Twitter
+          </a>
+        </Link>
+      </List>
+    </Container>
   )
 }
 
 export default App
+
+const Title = styled.h1`
+  font-size: 70px;
+  font-weight: 600;
+  line-height: 70px;
+
+  @media (max-width: 600px) {
+    font-size: 50px;
+  }
+`
+
+const Subtitle = styled.p`
+  font-size: 18px;
+  line-height: 25px;
+  margin-top: 20px;
+`
+
+const Container = styled.div`
+  font-family: 'Gilroy', sans-serif;
+  width: 50%;
+  min-width: 500px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  @media (max-width: 600px) {
+    width: 90%;
+    min-width: unset;
+  }
+`
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 100px;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+`
+
+const Link = styled.li`
+  overflow: hidden;
+  a {
+    display: inline-block;
+    color: #00df94;
+    text-decoration: none;
+    transition: transform 0.4s;
+    font-size: 18px;
+    font-weight: 600;
+    position: relative;
+  }
+  a::before {
+    content: attr(data-text);
+    position: absolute;
+    top: 100%;
+  }
+  a:hover {
+    transform: translateY(-100%);
+  }
+
+  @media (max-width: 600px) {
+    margin-top: 15px;
+  }
+`
